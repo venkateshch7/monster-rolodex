5fe1e6c0a5b10423e2c18c8b5cc3fc1cbe6f23d2
venkatesh ch <chvenkatesh255@gmail.com>
2020-04-05 00:56:26 +0530

    Updates

---
+++ precache-manifest.b5e7fbd9be30a78e06e1ba0b23a0627a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77ab1ef92eb06db608089a1cb68e4ba3",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "87670d084f23e6043d4c",
+    "url": "/monster-rolodex/static/css/main.25270dc9.chunk.css"
+  },
+  {
+    "revision": "f3bc58ad93c95ac2dfd4",
+    "url": "/monster-rolodex/static/js/2.7158d2bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.7158d2bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87670d084f23e6043d4c",
+    "url": "/monster-rolodex/static/js/main.86780d21.chunk.js"
+  },
+  {
+    "revision": "00719569f4c4503b3323",
+    "url": "/monster-rolodex/static/js/runtime-main.68db6682.js"
+  }
+]);
